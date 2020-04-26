424f70b5ad9bef15d786a47763813acc02952185
tangtanglove <dai_hang_love@126.com>
2020-04-26 13:40:53 +0800

    提交代码

--- config/quark.php
+++ config/quark.php
@@ -8,7 +8,7 @@ return [
     ],
 
     'admin' => [
-        'version' => env('ADMIN_VERSION', 'v0.0.4'),
+        'version' => env('ADMIN_VERSION', 'v0.0.5'),
         'name' => env('ADMIN_NAME', 'Quark-Admin')
     ],
 
