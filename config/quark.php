bcd09c10953e3b4f9e0c8eb7a4f1048d7a39e8f8
tangtanglove <l>
2020-07-08 19:41:17 +0800

    修复一个bug

--- config/quark.php
+++ config/quark.php
@@ -8,7 +8,7 @@ return [
     ],
 
     'admin' => [
-        'version' => env('ADMIN_VERSION', 'v0.1.1'),
+        'version' => env('ADMIN_VERSION', 'v0.1.2'),
         'name' => env('ADMIN_NAME', 'Quark-Admin')
     ],
 
