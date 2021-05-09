e4a2ad617abb691ffd270fb746476a79e72ada71
quarkcms <60868863+quarkcms@users.noreply.github.com>
2021-05-09 19:20:01 +0800

    wip

--- src/Http/Resources/AdminCreateResource.php
+++ src/Http/Resources/AdminCreateResource.php
@@ -32,31 +32,24 @@ class AdminCreateResource extends FormResource
         return [
             FormItem::image('avatar','头像'),
             FormItem::text('username','用户名')->rules(
-                ['required','min:6','max:20'],
-                ['required'=>'用户名必须填写','min'=>'用户名不能少于6个字符','max'=>'用户名不能超过20个字符']
+                [
+                    'required',
+                    'min:6',
+                    'max:20'
+                ],
+                [
+                    'required' => '用户名必须填写',
+                    'min' => '用户名不能少于6个字符',
+                    'max' => '用户名不能超过20个字符'
+                ]
             ),
             FormItem::checkbox('role_ids','角色')->options($data['roles']),
-            FormItem::text('nickname','昵称')->rules(
-                ['required'],
-                ['required'=>'昵称必须填写']
-            ),
-            FormItem::text('email','邮箱')->rules(
-                ['required'],
-                ['required'=>'邮箱必须填写']
-            ),
-            FormItem::text('phone','手机号')->rules(
-                ['required'],
-                ['required'=>'手机号必须填写']
-            ),
+            FormItem::text('nickname','昵称')->rules(['required'], ['required' => '昵称必须填写']),
+            FormItem::text('email','邮箱')->rules(['required'], ['required' => '邮箱必须填写']),
+            FormItem::text('phone','手机号')->rules(['required'], ['required' => '手机号必须填写']),
             FormItem::radio('sex','性别')->options([1 => '男', 2 => '女'])->value(1),
-            FormItem::password('password','密码')->rules(
-                ['required'],
-                ['required'=>'密码必须填写']
-            ),
-            FormItem::switch('status','状态')->options([
-                'on'  => '正常',
-                'off' => '禁用'
-            ])->value(true)
+            FormItem::password('password','密码')->rules(['required'], ['required'=>'密码必须填写']),
+            FormItem::switch('status','状态')->options(['on'  => '正常','off' => '禁用'])->value(true)
         ];
     }
 }
