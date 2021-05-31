df51f270baef8d78d4e7ff337386a883b0c8911a
quarkcms <60868863+quarkcms@users.noreply.github.com>
2021-05-31 18:21:16 +0800

    wip

--- src/Console/stubs/Resources/Admin.php
+++ src/Console/stubs/Resources/Admin.php
@@ -2,7 +2,10 @@
 
 namespace App\Admin\Resources;
 
+use Illuminate\Http\Request;
 use QuarkCMS\QuarkAdmin\Resource;
+use QuarkCMS\QuarkAdmin\Field;
+use Spatie\Permission\Models\Role;
 
 class Admin extends Resource
 {
@@ -21,15 +24,35 @@ class Admin extends Resource
     public static $model = 'QuarkCMS\QuarkAdmin\Models\Admin';
 
     /**
-     * 字段
+     * 分页
+     *
+     * @var int|bool
+     */
+    public static $pagination = 10;
+
+    /**
+     * 查询
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Laravel\Scout\Builder  $query
+     * @return \Laravel\Scout\Builder
+     */
+    public static function scoutQuery(Request $request, $query)
+    {
+        return $query;
+    }
+
+    /**
+     * 过滤器
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function fields(Request $request)
+    public function filters(Request $request)
     {
         return [
-            
+            new Filters\TimestampFilter('created_at'),
+            new Filters\TimestampFilter('deleted_at'),
         ];
     }
 
@@ -44,5 +67,50 @@ class Admin extends Resource
         return [
 
         ];
+    }
+
+    /**
+     * 字段
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        $roles = $this->getRoles();
+
+        return [
+            Field::image('avatar','头像')->onlyOnForms(),
+            Field::text('username','用户名')->rules(
+                ['required','min:6','max:20'],
+                ['required' => '用户名必须填写','min' => '用户名不能少于6个字符','max' => '用户名不能超过20个字符']
+            ),
+            Field::checkbox('role_ids','角色')->options($roles)->onlyOnForms(),
+            Field::text('nickname','昵称')->rules(['required'], ['required' => '昵称必须填写']),
+            Field::text('email','邮箱')->rules(['required'], ['required' => '邮箱必须填写']),
+            Field::text('phone','手机号')->rules(['required'], ['required' => '手机号必须填写']),
+            Field::radio('sex','性别')->options([1 => '男', 2 => '女'])->default(1),
+            Field::password('password','密码')->rules(['required'], ['required'=>'密码必须填写'])->onlyOnForms(),
+            Field::datetime('last_login_time','最后登录时间')->onlyOnIndex(),
+            Field::switch('status','状态')->options(['on'  => '正常','off' => '禁用'])->default(true),
+        ];
+    }
+
+    /**
+     * 获取角色
+     *
+     * @param  void
+     * @return array
+     */
+    protected function getRoles()
+    {
+        $getRoles = Role::where('guard_name','admin')->get()->toArray();
+        $roles = [];
+
+        foreach ($getRoles as $key => $role) {
+            $roles[$role['id']] = $role['name'];
+        }
+
+        return $roles;
     }
 }
--- src/Resource.php
+++ src/Resource.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use QuarkCMS\Quark\Facades\Form;
 use QuarkCMS\Quark\Facades\FormItem;
 use QuarkCMS\Quark\Facades\Table;
+use QuarkCMS\Quark\Facades\Column;
+use QuarkCMS\Quark\Facades\Action;
 
 /**
  * Class Resource.
@@ -13,6 +15,13 @@ use QuarkCMS\Quark\Facades\Table;
 abstract class Resource
 {
     use Layout;
+
+    /**
+     * 分页
+     *
+     * @var int|bool
+     */
+    public static $pagination = false;
 
     /**
      * Get a fresh instance of the model represented by the resource.
@@ -27,6 +36,77 @@ abstract class Resource
     }
 
     /**
+     * 配置分页
+     *
+     * @return mixed
+     */
+    public static function pagination()
+    {
+        return static::$pagination;
+    }
+
+    /**
+     * 将字段转换表格列
+     *
+     * @param  Request $request
+     * @return array
+     */
+    protected function fieldsToColumns($request)
+    {
+        $fields = $this->fields($request);
+        $columns = [];
+
+        foreach ($fields as $key => $value) {
+            if($value->showOnIndex) {
+                $column = Column::make($value->name, $value->label);
+
+                if(isset($value->options)) {
+                    $options = [];
+                    if(in_array($value->type, ['checkbox', 'radio', 'select'])) {
+                        foreach ($value->options as $optionKey => $optionValue) {
+                            $options[$optionValue['value']] = $optionValue['label'];
+                        }
+                    } else {
+                        $options = $value->options;
+                    }
+                    $column->valueEnum($options);
+                }
+
+                if($value->type === 'datetime') {
+                    $columns[] = Column::make($value->name.'_range', $value->label)->hideInTable()->valueType('dateTimeRange')->render();
+                    $column->hideInSearch()->valueType('dateTime');
+                }
+
+                if($value->type === 'date') {
+                    $columns[] = Column::make($value->name.'_range', $value->label)->hideInTable()->valueType('dateRange')->render();
+                    $column->hideInSearch()->valueType('date');
+                }
+
+                $columns[] = $column->render();
+            }
+        }
+
+        return $columns;
+    }
+
+    /**
+     * 获取表格数据
+     *
+     * @param  Request $request
+     * @return array
+     */
+    protected function getData($request)
+    {
+        $model = static::newModel();
+
+        if(static::pagination()) {
+            return $model->paginate(request('pageSize',static::pagination()));
+        } else {
+            return $model->get()->toArray();
+        }
+    }
+
+    /**
      * 列表资源
      *
      * @param  void
@@ -34,15 +114,19 @@ abstract class Resource
      */
     public function indexResource(Request $request)
     {
-        $model = static::newModel();
-
-        $fields = $this->fields($request);
+        $data = $this->getData($request);
 
         $table = Table::key('table')
         ->title($this->title)
         ->toolBar(false)
-        ->columns([])
+        ->columns($this->fieldsToColumns($request))
         ->batchActions([]);
+
+        if(static::pagination()) {
+            $table = $table->datasource($data->items())->pagination($data->currentPage(), $data->perPage(), $data->total());
+        } else {
+            $table = $table->datasource($data);
+        }
 
         return $this->setLayoutContent($table);
     }
